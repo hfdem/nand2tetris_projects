c182baf92b4d38a71d333e38e22e97bc3fb03346
hfdem <yangruipengchn@gmail.com>
2022-02-01 18:29:46 +0800

    project04: Complete Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,58 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+//  Put your code here.
+    @SCREEN
+    D = A
+    @addr
+    M = D
+
+(DETEC_KBD)
+    @n
+    M = 0   // n = 0
+    @KBD
+    D = M
+    @SCREEN_BLACK
+    D; JGT  // if KBD > 0 goto SCREEN_BLACK
+    @n
+    M = 0   // n = 0
+    @SCREEN_WHITE
+    D; JEQ  //if KBD == 0 goto SCREEN_WHITE
+    @DETEC_KBD
+    0; JMP
+
+(SCREEN_BLACK)
+    @n
+    D = M
+    @8191   // 512 pixel * 256 rows // 16 - 1 = 8191
+    D = D - A
+    @DETEC_KBD
+    D; JGT  // if n > 8191 goto END
+    @addr
+    D = M
+    @n
+    D = D + M
+    A = D
+    M = -1  // MEM[addr + n] = -1
+    @n
+    M = M + 1   // n++
+    @SCREEN_BLACK   // goto SCREEN_BLACK
+    0; JMP
+
+(SCREEN_WHITE)
+    @n
+    D = M
+    @8191   // 512 pixel * 256 rows // 16 - 1 = 8191
+    D = D - A
+    @DETEC_KBD
+    D; JGT  // if n > 8191 goto END
+    @addr
+    D = M
+    @n
+    D = D + M
+    A = D
+    M = 0   // MEM[addr + n] = 0
+    @n
+    M = M + 1   // n++
+    @SCREEN_WHITE   // goto SCREEN_WHITE
+    0; JMP
