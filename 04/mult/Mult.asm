3b85e4f079502fd311f06e357ee811c0da9ada03
hfdem <yangruipengchn@gmail.com>
2022-02-01 16:49:08 +0800

    project04: Complete Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,59 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
+//     i = 1
+//     value = R0
+//     n = R1
+//     sum = 0
+//     R2 = 0
+// LOOP:
+//     if i > n goto STOP
+//     sum = sum + value
+//     i++ 
+//     goto LOOP
+// STOP:
+//     R2 = sum
+
 // Put your code here.
+    @i
+    M = 1
+    @R0
+    D = M
+    @value
+    M = D
+    @R1
+    D = M
+    @n
+    M = D
+    @sum
+    M = 0
+    @R2
+    M = 0
+
+(LOOP)
+    @i
+    D = M
+    @n
+    D = D - M
+    @STOP
+    D; JGT  // if i > n goto STOP
+    @sum
+    D = M
+    @value
+    D = D + M
+    @sum
+    M = D   // sum = sum + value
+    @i
+    M = M + 1   // i++
+    @LOOP
+    0; JMP
+
+(STOP)
+    @sum
+    D = M
+    @R2
+    M = D   // R2 = sum
+
+(END)
+    @END
+    0; JMP
